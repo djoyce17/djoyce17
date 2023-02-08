5b9a646eee702e1ff9bc29e4c8f83dca2660838e
djaycat <54156037+djoyce17@users.noreply.github.com>
2023-02-08 11:32:47 -0500

    Update understood_slide_deck_analysis.sql

--- understood_slide_deck_analysis.sql
+++ understood_slide_deck_analysis.sql
@@ -1,64 +1,47 @@
 -- --number of users
--- select count(distinct understood_id)
--- from pageview_data
+select count(distinct understood_id)
+from pageview_data
 -- --10000
 
 -- --numbner of users by device
--- with r as(
--- select 
---     device_class
---     , count(distinct understood_id) as users_accessed
--- from pageview_data
--- group by 1)
+with r as(
+select 
+    device_class
+    , count(distinct understood_id) as users_accessed
+from pageview_data
+group by 1)
 
 -- --number of users who use more than 1 device to access the article
--- select understood_id, 
--- count(distinct device_class) as counted
--- from pageview_data 
--- group by 1 
--- having count(distinct device_class) > 1
--- order by 2 desc
+select understood_id, 
+count(distinct device_class) as counted
+from pageview_data 
+group by 1 
+having count(distinct device_class) > 1
+order by 2 desc
 
 -- -- what are the top 5 articles in the time period?
--- with s as (
---     select page_path_slug,
---     count(*) as num
---     from pageview_data p
---     where 1=1
---     and site_section = 'articles'
---     group by 1
+with s as (
+    select page_path_slug,
+    count(*) as num
+    from pageview_data p
+    join session_data s
+    on p.session_id = s.session_id
+    where 1=1
+    and site_section = 'articles'
+    and not engagement_is_bounce_session
+    group by 1
 
--- )
+)
 
--- select * 
--- from
---     (
---         select 
---             s.*
---             , row_number() OVER ( order by num desc) as rank 
---         from s 
---     ) r
--- where rank <=5
+select * 
+from
+    (
+        select 
+            s.*
+            , row_number() OVER ( order by num desc) as rank 
+        from s 
+    ) r
+where rank <=5
 
--- -- what about over time. may not use this
--- with s as (
---     select page_path_slug,
---     date_part('month',pageview_date_est) as tmonth,
---     count(*) as num
---     from pageview_data p
---     where 1=1
---     and site_section = 'articles'
---     group by 1,2
 
--- )
-
--- select * 
--- from
---     (
---         select 
---             s.*
---             , row_number() OVER (partition by tmonth order by num desc) as rank 
---         from s 
---     ) r
--- where rank <=5
 
